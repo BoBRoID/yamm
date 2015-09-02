a8deae7a0e811a3107aea56f83ac5b0629749033
BoBRoID <n.gilko@gmail.com>
2015-09-02 13:11:38 +0300

    change some tags

--- Yamm.php
+++ Yamm.php
@@ -76,10 +76,15 @@ class Yamm extends Widget
         $link = empty($link) ? '#' : $link;
 
         if(preg_match('/[\w\-]+\.(jpg|png|gif|jpeg)/', $content)){
-            $content = '<img src="'.$content.'">';
+            $content = Html::tag('img', '', [
+                'src'   =>  $content
+            ]);
         }
 
-        return '<a class="cd-logo" href="'.$link.'">'.$content.'</a>';
+        return Html::tag('a', $content, [
+            'class' =>  'cd-logo',
+            'href'  =>  $link
+        ]);
     }
 
     /**
@@ -135,305 +140,42 @@ class Yamm extends Widget
     }
 
     protected function renderMenu(){
-        $header = Html::tag('header', $this->renderLogo().'
-    <ul class="cd-header-buttons">
-        <li><a class="cd-search-trigger" href="#cd-search">'.$this->options['searchLabel'].'<span></span></a></li>
-        <li><a class="cd-nav-trigger" href="#cd-primary-nav">'.$this->options['menuLabel'].'<span></span></a></li>
-    </ul>', $this->options['headerOptions']);
-
-        $header .= '<div class="cd-overlay"></div>';
-        $header .= '<nav class="cd-nav">
-    <ul id="cd-primary-nav" class="cd-primary-nav is-fixed">';
+        $items = '';
 
         foreach($this->items as $item){
-            $header .= $this->renderItem($item);
+            $items .= $this->renderItem($item);
         }
 
-        /*$header .= '<li class="has-children">
-            <a href="http://codyhouse.co/?p=409">Clothing</a>
+        $header = Html::tag('header', $this->renderLogo().Html::tag('ul', Html::tag('li', Html::tag('a', $this->options['searchLabel'].Html::tag('span'), [
+                    'class' =>  'cd-search-trigger',
+                    'href'  =>  '#cd-search'
+                ])).Html::tag('li', Html::tag('a', $this->options['menuLabel'].Html::tag('span'), [
+                    'class' =>  'cd-nav-trigger',
+                    'href'  =>  '#cd-primary-nav'
+                ])), [
+                'class' =>  'cd-header-buttons'
+            ]), $this->options['headerOptions']);
 
-            <ul class="cd-secondary-nav is-hidden">
-                <li class="go-back"><a href="#0">Menu</a></li>
-                <li class="see-all"><a href="http://codyhouse.co/?p=409">All Clothing</a></li>
-                <li class="has-children">
-                    <a href="http://codyhouse.co/?p=409">Accessories</a>
+        $overlay = Html::tag('div', '', [
+            'class' =>  'cd-overlay'
+        ]);
 
-                    <ul class="is-hidden">
-                        <li class="go-back"><a href="#0">Clothing</a></li>
-                        <li class="see-all"><a href="http://codyhouse.co/?p=409">All Accessories</a></li>
-                        <li class="has-children">
-                            <a href="#0">Beanies</a>
+        $nav = Html::tag('nav', Html::tag('ul', $items, [
+            'class' =>  'cd-primary-nav is-fixed',
+            'id'    =>  'cd-primary-nav'
+        ]), [
+            'class' =>  'cd-nav'
+        ]);
 
-                            <ul class="is-hidden">
-                                <li class="go-back"><a href="#0">Accessories</a></li>
-                                <li class="see-all"><a href="http://codyhouse.co/?p=409">All Benies</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Caps &amp; Hats</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Gifts</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Scarves &amp; Snoods</a></li>
-                            </ul>
-                        </li>
-                        <li class="has-children">
-                            <a href="#0">Caps &amp; Hats</a>
+        $search = Html::tag('div', Html::tag('form', Html::tag('input', '', [
+            'type'          =>  'search',
+            'placeholder'   =>  isset($this->options['searchPlaceholder']) ? $this->options['searchPlaceholder'] : 'Поиск...'
+        ])), [
+            'class' =>  'cd-search',
+            'id'    =>  'cd-search'
+        ]);
 
-                            <ul class="is-hidden">
-                                <li class="go-back"><a href="#0">Accessories</a></li>
-                                <li class="see-all"><a href="http://codyhouse.co/?p=409">All Caps &amp; Hats</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Beanies</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Caps</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Hats</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="http://codyhouse.co/?p=409">Glasses</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Gloves</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Jewellery</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Scarves</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Wallets</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Watches</a></li>
-                    </ul>
-                </li>
-
-                <li class="has-children">
-                    <a href="http://codyhouse.co/?p=409">Bottoms</a>
-
-                    <ul class="is-hidden">
-                        <li class="go-back"><a href="#0">Clothing</a></li>
-                        <li class="see-all"><a href="http://codyhouse.co/?p=409">All Bottoms</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Casual Trousers</a></li>
-                        <li class="has-children">
-                            <a href="#0">Jeans</a>
-
-                            <ul class="is-hidden">
-                                <li class="go-back"><a href="#0">Bottoms</a></li>
-                                <li class="see-all"><a href="http://codyhouse.co/?p=409">All Jeans</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Ripped</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Skinny</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Slim</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Straight</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#0">Leggings</a></li>
-                        <li><a href="#0">Shorts</a></li>
-                    </ul>
-                </li>
-
-                <li class="has-children">
-                    <a href="http://codyhouse.co/?p=409">Jackets</a>
-
-                    <ul class="is-hidden">
-                        <li class="go-back"><a href="#0">Clothing</a></li>
-                        <li class="see-all"><a href="http://codyhouse.co/?p=409">All Jackets</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Blazers</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Bomber jackets</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Denim Jackets</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Duffle Coats</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Leather Jackets</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Parkas</a></li>
-                    </ul>
-                </li>
-
-                <li class="has-children">
-                    <a href="http://codyhouse.co/?p=409">Tops</a>
-
-                    <ul class="is-hidden">
-                        <li class="go-back"><a href="#0">Clothing</a></li>
-                        <li class="see-all"><a href="http://codyhouse.co/?p=409">All Tops</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Cardigans</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Coats</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Hoodies &amp; Sweatshirts</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Jumpers</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Polo Shirts</a></li>
-                        <li><a href="http://codyhouse.co/?p=409">Shirts</a></li>
-                        <li class="has-children">
-                            <a href="#0">T-Shirts</a>
-
-                            <ul class="is-hidden">
-                                <li class="go-back"><a href="#0">Tops</a></li>
-                                <li class="see-all"><a href="http://codyhouse.co/?p=409">All T-shirts</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Plain</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Print</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Striped</a></li>
-                                <li><a href="http://codyhouse.co/?p=409">Long sleeved</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="http://codyhouse.co/?p=409">Vests</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-
-        <li class="has-children">
-            <a href="http://codyhouse.co/?p=409">Gallery</a>
-
-            <ul class="cd-nav-gallery is-hidden">
-                <li class="go-back"><a href="#0">Menu</a></li>
-                <li class="see-all"><a href="http://codyhouse.co/?p=409">Browse Gallery</a></li>
-                <li>
-                    <a class="cd-nav-item" href="http://codyhouse.co/?p=409">
-                        <img src="img/img.jpg" alt="Product Image">
-                        <h3>Product #1</h3>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item" href="http://codyhouse.co/?p=409">
-                        <img src="img/img.jpg" alt="Product Image">
-                        <h3>Product #2</h3>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item" href="http://codyhouse.co/?p=409">
-                        <img src="img/img.jpg" alt="Product Image">
-                        <h3>Product #3</h3>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item" href="http://codyhouse.co/?p=409">
-                        <img src="img/img.jpg" alt="Product Image">
-                        <h3>Product #4</h3>
-                    </a>
-                </li>
-            </ul>
-        </li>
-
-        <li class="has-children">
-            <a href="http://codyhouse.co/?p=409">Services</a>
-            <ul class="cd-nav-icons is-hidden">
-                <li class="go-back"><a href="#0">Menu</a></li>
-                <li class="see-all"><a href="http://codyhouse.co/?p=409">Browse Services</a></li>
-                <li>
-                    <a class="cd-nav-item item-1" href="http://codyhouse.co/?p=409">
-                        <h3>Service #1</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-2" href="http://codyhouse.co/?p=409">
-                        <h3>Service #2</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-3" href="http://codyhouse.co/?p=409">
-                        <h3>Service #3</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-4" href="http://codyhouse.co/?p=409">
-                        <h3>Service #4</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-5" href="http://codyhouse.co/?p=409">
-                        <h3>Service #5</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-6" href="http://codyhouse.co/?p=409">
-                        <h3>Service #6</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-7" href="http://codyhouse.co/?p=409">
-                        <h3>Service #7</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-8" href="http://codyhouse.co/?p=409">
-                        <h3>Service #8</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-            </ul>
-        </li>
-
-        <li class="has-children">
-            <a href="http://codyhouse.co/?p=409">Services</a>
-            <ul class="cd-nav-icons is-hidden">
-                <li class="go-back"><a href="#0">Menu</a></li>
-                <li class="see-all"><a href="http://codyhouse.co/?p=409">Browse Services</a></li>
-                <li>
-                    <a class="cd-nav-item item-1" href="http://codyhouse.co/?p=409">
-                        <h3>Service #1</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-2" href="http://codyhouse.co/?p=409">
-                        <h3>Service #2</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-3" href="http://codyhouse.co/?p=409">
-                        <h3>Service #3</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-4" href="http://codyhouse.co/?p=409">
-                        <h3>Service #4</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-5" href="http://codyhouse.co/?p=409">
-                        <h3>Service #5</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-6" href="http://codyhouse.co/?p=409">
-                        <h3>Service #6</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-7" href="http://codyhouse.co/?p=409">
-                        <h3>Service #7</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-
-                <li>
-                    <a class="cd-nav-item item-8" href="http://codyhouse.co/?p=409">
-                        <h3>Service #8</h3>
-                        <p>This is the item description</p>
-                    </a>
-                </li>
-            </ul>
-        </li>
-
-        <li><a href="http://codyhouse.co/?p=409">Standard</a></li>';
-*/
-$header .= '</ul>
-</nav>
-<div id="cd-search" class="cd-search">
-    <form>
-        <input type="search" placeholder="Search...">
-    </form>
-</div>';
-
-        return $header;
+        return $header.$overlay.$nav.$search;
     }
 
     public function run(){
