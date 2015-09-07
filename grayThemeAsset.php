f4cc0bed3e962f3b9074514f4aca17c795337d91
BoBRoID <n.gilko@gmail.com>
2015-09-07 11:51:41 +0300

    themes mech

---
+++ grayThemeAsset.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bobroid
+ * Date: 07.09.15
+ * Time: 11:51
+ */
+
+namespace bobroid\yamm;
+
+
+class grayThemeAsset extends \yii\web\AssetBundle{
+
+}
