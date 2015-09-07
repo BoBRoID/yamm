268e4eb18b5945c916965da093808e20d11d86b1
BoBRoID <n.gilko@gmail.com>
2015-09-07 11:58:01 +0300

    themes mech

--- grayThemeAsset.php
+++ grayThemeAsset.php
@@ -11,4 +11,18 @@ namespace bobroid\yamm;
 
 class grayThemeAsset extends \yii\web\AssetBundle{
 
+    public $depends = [
+        'bobroid\yamm\YammAsset',
+    ];
+
+    public $sourcePath = '@vendor/bobroid/yamm/assets/themes/gray';
+
+    public $css = [
+        'css/theme.css',
+    ];
+
+    public $js = [
+        
+    ];
+
 }
